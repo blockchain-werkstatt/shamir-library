c34569a0b301a1704ad77f0ae7556264a37d768c
hsi <har.preet.singh@fokus.fraunhofer.de>
2018-09-12 17:17:23 +0200

    bigprime temp removed

--- src/core/hex.js
+++ src/core/hex.js
@@ -1,11 +1,5 @@
 const Buffer = require('buffer/').Buffer
 
-//for the instantiation
-
-const bigPrime = () =>'0x7465737475626720697320776f726b696e67'
-const bigString = () =>'abcasdjdgsdklgjgkj3kjglksjsklkj3kjlkj23lkjgkljg';
-
-
 const encode = (text)=>{
     return '0x'+Buffer.from(text).toString("hex");
 }
@@ -17,7 +11,5 @@ const decode = (hex)=>{
 
 module.exports = {
     encode,
-    decode,
-    bigPrime,
-    bigString
+    decode
 }
